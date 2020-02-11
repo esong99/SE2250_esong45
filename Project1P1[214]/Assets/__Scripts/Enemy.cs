bedaa9f0fbf28b0a18cf254bd7e98a58322dd71a
robotmaster99 <60909966+robotmaster99@users.noreply.github.com>
2020-02-11 09:45:25 -0500

    first commit

---
+++ Project1P1[214]/Assets/__Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int HealthPoint = 100;
+
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(HealthPoint <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        HealthPoint -= damage;
+    }
+}
---
+++ Project1P1[214]/Assets/__Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public LayerMask damaged;
+    public Transform damageCheck;
+    public float DamageRadius = .2f;
+    public int damage = 5;
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D[] colliderD = Physics2D.OverlapCircleAll(damageCheck.position, DamageRadius, damaged);
+        for (int i = 0; i < colliderD.Length; i++)
+        {
+            if (colliderD[i].gameObject == gameObject)
+            {
+                TakeDamage(damage);
+            }
+        }
+        if (currentHealth == 0)
+        {
+            Invoke("Restart", 3.0f);
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+
+    }
+    void Restart()
+    {
+        //restarting game
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
